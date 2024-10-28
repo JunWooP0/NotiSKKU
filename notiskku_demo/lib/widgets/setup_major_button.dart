6d1c3cd1fca7eb458a0e033a491ad03187ba61fe
twjin03 <twjin03@g.skku.edu>
2024-10-28 21:07:16 +0900

    design: Change button to variable size

--- notiskku_demo/lib/widgets/setup_major_button.dart
+++ notiskku_demo/lib/widgets/setup_major_button.dart
@@ -6,25 +6,39 @@ class SetMajorButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const SelectMajorScreen(),
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: screenHeight * 0.02), // 하단 여백 추가
+      child: FractionallySizedBox(
+        widthFactor: 0.85, // 버튼 너비를 화면 너비의 85%로 설정
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SelectMajorScreen(),
+              ),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff0b5b42),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
           ),
-        );
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xff0b5b42),
-        padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 11), // 위쪽 여백
+              Text(
+                '학과/키워드 설정하기',
+                style: TextStyle(fontSize: 18, color: Colors.white), // 텍스트 크기 변경
+              ),
+              SizedBox(height: 11), // 아래쪽 여백
+            ],
+          ),
         ),
-      ),
-      child: const Text(
-        '학과/키워드 설정하기',
-        style: TextStyle(fontSize: 24, color: Colors.white),
       ),
     );
   }
